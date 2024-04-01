6346f0cede3a0a72b0694ce3c542cf4751eef467
georgia.antunes <georgiaantunes@outlook.com>
2024-04-01 10:03:20 -0300

    second implementation

--- reverseArray.js
+++ reverseArray.js
@@ -8,3 +8,17 @@ function reverseArray(A) {
 }
 
 console.log(reverseArray([1,2,3,4,5,6,7,8,9,10]));
+
+//--------------------------------------------------------------------------------
+// Second resolution
+function test(n){
+    let reverseArray = [];
+    for(let index = n.length-1; index >= 0; index-=1){
+        let content = n[index];
+        reverseArray.push(content);
+    }
+    return reverseArray;
+}
+
+console.log(
+    test([1,3,4,10,7]));
